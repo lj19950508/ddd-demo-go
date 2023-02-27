38a73a7d3aacdca4c8003d5e1e6ae6c8daedb3fd
Gon <156441588@qq.com>
2023-02-28 02:02:43 +0800

    w

--- internal/app/wire.go
+++ internal/app/wire.go
@@ -1,12 +1,10 @@
 package app
 
+//区分什么是不运行就关掉的   redis不必须， mysql 必须  ,mq（可选）必须。。等等  必须的 要监听错误的话得关闭当前整个服务以便于被观测到， 或者得发送通知，
 //只会在两个地方register  wire和main
 //注册的东西有service, repository , mysql
 //在api wire service,或者 也是在adapter wire别的东西，
 
-func wire(){
-
-
-	
+func wire() {
 
 }
